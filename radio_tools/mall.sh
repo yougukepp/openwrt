d46ae9a95ed10fac78e3b4ec297c0f9c21f31609
PengPeng <pengpeng@fiberhome.com>
2015-03-31 21:28:47 +0800

    update mall.sh

--- radio_tools/mall.sh
+++ radio_tools/mall.sh
@@ -4,7 +4,7 @@
 cp ./radio_tools/5350.config ./.config
 
 make menuconfig
-make -j8 #V=99
+make -j4 #V=99
 
 cp ./bin/ramips/openwrt-ramips-rt305x-mpr-a2-squashfs-sysupgrade.bin /tftp/rt5350
 chmod 777 /tftp/rt5350
