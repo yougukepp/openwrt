084a88926c73e59fbef589914f6c23891985efa5
PengPeng <pengpeng@fiberhome.com>
2015-03-31 20:44:39 +0800

    使用tftp目录

--- radio_tools/mall.sh
+++ radio_tools/mall.sh
@@ -6,8 +6,8 @@ cp ./radio_tools/5350.config ./.config
 make menuconfig
 make -j8 #V=99
 
-#cp ./bin/ramips/openwrt-ramips-rt305x-mpr-a2-squashfs-sysupgrade.bin /var/www/ftp/rt5350
-#chmod 777 /var/www/ftp/rt5350
+cp ./bin/ramips/openwrt-ramips-rt305x-mpr-a2-squashfs-sysupgrade.bin /tftp/rt5350
+chmod 777 /tftp/rt5350
 
-#rm -rf ./bin/*
+rm -rf ./bin/*
 
