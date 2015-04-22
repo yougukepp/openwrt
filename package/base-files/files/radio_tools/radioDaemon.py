98e05bab53488cfe22cd17f1ff9218ef9eb1b64f
PengPeng <pengpeng@fiberhome.com>
2015-04-22 20:39:10 +0800

    modify default python daemon

--- package/base-files/files/radio_tools/radioDaemon.py
+++ package/base-files/files/radio_tools/radioDaemon.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+import socket
 
+g_sleep_time = 300
 
-def HelloToWife():
-    print("/etc/radioDaemon.conf 文件内容:")
-    print
+g_in_ip = '192.168.10.1'
+g_in_port = 8001
+g_in_addr = (g_in_ip, g_in_port)
+g_udp_in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+g_udp_in_socket.bind(g_in_addr)
 
-    # 准备解析配置文件
-    f = open("../etc/radioDaemon.conf")
+g_out_ip = '192.168.2.100'
+g_out_port = 8001
+g_out_addr = (g_out_ip, g_out_port)
+g_udp_out_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
-    for l in f:
-        print l,
+g_buf_size = 1024
 
-    # 主模式 直接退出
-    # 从模式 修改配置文件后退出
+def router(): 
+    (recvData, clientAddr) = g_udp_in_socket.recvfrom(g_buf_size) 
+    print 'recv from:',
+    print clientAddr,
+    print 'data:',
+    print recvData
+
+    g_udp_out_socket.sendto(recvData, g_out_addr)
+    print 'send:',
+    print recvData,
+    print 'to:',
+    print g_out_addr
 
 if __name__ == '__main__':
-    HelloToWife()
-
+    print 'begin'
+    #time.sleep(g_sleep_time)
+    router()
+    print 'end'
 
